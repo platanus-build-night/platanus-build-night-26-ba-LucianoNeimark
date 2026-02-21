e8d3280727b65797f86df9b6c6f955e5c1ecc846
Luciano <38950566+LucianoNeimark@users.noreply.github.com>
2026-02-20 22:59:28 -0300

    Update test_calculator.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test_calculator.py
+++ test_calculator.py
@@ -15,4 +15,6 @@ def test_sum_zeros():
 
 
 def test_sum_floats():
-    pass
+    assert sum(0.1, 0.2) == pytest.approx(0.3)
+    assert sum(1.5, 2.5) == pytest.approx(4.0)
+    assert sum(-1.1, 2.2) == pytest.approx(1.1)
