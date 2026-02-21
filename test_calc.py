27b14005d19e70ba098cebdba74aee83c12e5fc4
Luciano <38950566+LucianoNeimark@users.noreply.github.com>
2026-02-21 00:33:52 -0300

    Update test_calc.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test_calc.py
+++ test_calc.py
@@ -3,7 +3,10 @@ from calc import *
 
 
 def test_multiply():
-    pass
+    assert multiply(3, 4) == 12
+    assert multiply(-2, 5) == -10
+    assert multiply(0, 99) == 0
+    assert multiply(-3, -7) == 21
 
 
 def test_divide():
